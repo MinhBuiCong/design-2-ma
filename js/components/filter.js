c9780e488acdab67e4a712c5b614ebd600daecc5
MinhBuiCong <62849019+MinhBuiCong@users.noreply.github.com>
2021-10-29 13:00:22 +0200

    Update filter.js

--- js/components/filter.js
+++ js/components/filter.js
@@ -74,6 +74,55 @@ export function filter() {
     <div class="input-range-container">
       <input type="text" placeholder="0" />
       <input type="text" placeholder="99999" />
+      </div>
     </div>
-  </div>`;
+    <div class="accordion accordion-flush" id="accordionFlushExample">
+    <div class="accordion-item">
+      <h2 class="accordion-header" id="flush-headingOne">
+        <button
+          class="accordion-button collapsed"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#flush-collapseOne"
+          aria-expanded="false"
+          aria-controls="flush-collapseOne"
+        >
+          Brands
+        </button>
+      </h2>
+      <div
+        id="flush-collapseOne"
+        class="accordion-collapse collapse"
+        aria-labelledby="flush-headingOne"
+        data-bs-parent="#accordionFlushExample"
+      >
+        <div class="accordion-body">Placeholder</div>
+      </div>
+    </div>
+  </div>
+    <div class="accordion accordion-flush" id="accordionFlushExample">
+    <div class="accordion-item">
+      <h2 class="accordion-header" id="flush-headingOne">
+        <button
+          class="accordion-button collapsed"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#flush-collapseOne"
+          aria-expanded="false"
+          aria-controls="flush-collapseOne"
+        >
+          Color
+        </button>
+      </h2>
+      <div
+        id="flush-collapseOne"
+        class="accordion-collapse collapse"
+        aria-labelledby="flush-headingOne"
+        data-bs-parent="#accordionFlushExample"
+      >
+        <div class="accordion-body">Placeholder</div>
+      </div>
+    </div>
+  </div>
+  `;
 }
