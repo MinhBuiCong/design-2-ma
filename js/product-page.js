f97c2300ef12fd69c32261555a17cf1f43fb85a7
MinhBuiCong <62849019+MinhBuiCong@users.noreply.github.com>
2021-10-29 12:45:22 +0200

    pagination

--- js/product-page.js
+++ js/product-page.js
@@ -3,9 +3,11 @@ import { breadcrumb } from "./components/breadcrumb.js";
 import { navBottom } from "./components/nav-bottom.js";
 import { filter } from "./components/filter.js";
 import { productCard } from "./components/product-card.js";
+import { pagination } from "./components/pagination.js";
 
 navbar();
 navBottom();
 breadcrumb();
 filter();
 productCard();
+pagination();
