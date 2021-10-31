82e8690b9cd168d8813ecf678f423728dc0f58ec
MinhBuiCong <62849019+MinhBuiCong@users.noreply.github.com>
2021-10-31 12:30:37 +0100

    new background picture

---
+++ icon/icon.js
@@ -0,0 +1,18 @@
+export const icon = {
+  left: `<svg xmlns="http://www.w3.org/2000/svg" width="22.054" height="22.037" viewBox="0 0 22.054 22.037">
+    <g id="arrow-right-circle" transform="translate(23.029 23.019) rotate(180)">
+      <path id="Path_3" data-name="Path 3" d="M10,0c2.64,0,6.673-.241,8.678,1.64C20.412,3.255,20,7.117,20,10c0,2.419.3,6.343-1.322,8.3C17.039,20.291,13.1,20,10,20c-2.811,0-6.748.388-8.772-1.7C-.318,16.719,0,12.712,0,10,0,7.609-.368,3.594,1.228,1.64,2.865-.38,6.869,0,10,0Z" transform="translate(2 2)" fill="none" stroke="#e26e49" stroke-linecap="round" stroke-width="2"/>
+      <path id="Path_2" data-name="Path 2" d="M12,16l4-4L12,8" fill="none" stroke="#e26e49" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"/>
+      <line id="Line_2" data-name="Line 2" x2="8" transform="translate(8 12)" fill="none" stroke="#e26e49" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"/>
+    </g>
+  </svg>
+  `,
+  right: `<svg xmlns="http://www.w3.org/2000/svg" width="22.054" height="22.037" viewBox="0 0 22.054 22.037">
+  <g id="arrow-right-circle" transform="translate(-0.975 -0.982)">
+    <path id="Path_3" data-name="Path 3" d="M10,0c2.64,0,6.673-.241,8.678,1.64C20.412,3.255,20,7.117,20,10c0,2.419.3,6.343-1.322,8.3C17.039,20.291,13.1,20,10,20c-2.811,0-6.748.388-8.772-1.7C-.318,16.719,0,12.712,0,10,0,7.609-.368,3.594,1.228,1.64,2.865-.38,6.869,0,10,0Z" transform="translate(2 2)" fill="none" stroke="#e26e49" stroke-linecap="round" stroke-width="2"/>
+    <path id="Path_2" data-name="Path 2" d="M12,16l4-4L12,8" fill="none" stroke="#e26e49" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"/>
+    <line id="Line_2" data-name="Line 2" x2="8" transform="translate(8 12)" fill="none" stroke="#e26e49" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"/>
+  </g>
+</svg>
+`,
+};
--- js/components/pagination.js
+++ js/components/pagination.js
@@ -1,15 +1,17 @@
+import { icon } from "../../icon/icon.js";
+
 export function pagination() {
   const paginationContainer = document.querySelector(".pagination-container");
   paginationContainer.innerHTML = `<nav aria-label="Page navigation example">
     <ul class="pagination justify-content-center">
-      <li class="page-item disabled">
-        <a class="page-link">Previous</a>
+      <li class="page-item">
+        <a class="page-link back-btn" href="#">${icon.left}</a>
       </li>
       <li class="page-item"><a class="page-link" href="#">1</a></li>
       <li class="page-item"><a class="page-link" href="#">2</a></li>
       <li class="page-item"><a class="page-link" href="#">3</a></li>
       <li class="page-item">
-        <a class="page-link" href="#">Next</a>
+        <a class="page-link" href="#">${icon.right}</a>
       </li>
     </ul>
   </nav>`;
